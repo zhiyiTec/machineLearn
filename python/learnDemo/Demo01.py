01801f782a73dc59247197e3084d687ef4077b9a
zhiyi <zhiyitec@qq.com>
2020-11-20 21:15:00 +0800

    新增python学习demo

---
+++ python/learnDemo/Demo01.py
@@ -0,0 +1,11 @@
+# TempStr = input("请输入带符号的温度值:")
+# if TempStr[-1] in ['F', 'f']:
+#     C = (eval(TempStr[0:-1]) - 32) / 1.8
+#     print("转换后的温度为：{:.2f}C".format(C))
+# elif TempStr[-1] in ['C', 'c']:
+#     F = 1.8 * eval(TempStr[0:-1]) + 32
+#     print("转换后的温度为：{:.2f}C".format(F))
+# else:
+#     print("输入格式错误")
+print(eval('"1+2"'))
+eval('print("Hello")')
---
+++ python/learnDemo/Demo02.py
@@ -0,0 +1,32 @@
+import turtle
+
+# '''
+# 用于设置启动窗体的大小以及位置
+# 650:绘图窗体本身的宽度
+# 350:绘图窗体本身的高度
+# 200:相对于屏幕左上角的x坐标
+# 300:相对于屏幕左上角y轴的坐标
+# '''
+# turtle.setup(650, 350, 200, 200)
+# turtle.penup()
+# turtle.fd(-250)
+# turtle.pendown()
+# turtle.pensize(25)
+# turtle.pencolor("purple")
+# turtle.seth(-40)
+# for i in range(4):
+#     turtle.circle(40, 80)
+#     turtle.circle(-40, 80)
+# turtle.circle(40, 80 / 2)
+# turtle.fd(40)
+# turtle.circle(16, 180)
+# turtle.fd(40 * 2 / 3)
+# turtle.done()
+
+turtle.left(45)
+turtle.fd(150)
+turtle.right(135)
+turtle.fd(300)
+turtle.left(135)
+turtle.fd(150)
+turtle.done()
