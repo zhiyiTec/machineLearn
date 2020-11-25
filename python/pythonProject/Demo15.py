52966bccfc996b4a10c00c72b766f44d2a39289c
zhiyi <zhiyitec@qq.com>
2020-11-25 21:11:47 +0800

    Demo15.py

---
+++ python/pythonProject/Demo15.py
@@ -0,0 +1,37 @@
+# A = {"python", 123, ("python", 123)}
+# print(A)
+# B = set("pypy123123")
+# print(B)
+# C = {"python", 123, "python", 123}
+# print(C)
+
+# A = {"p", "y", 123}
+# B = set("pypym123")
+# print("A-B:{}".format(A - B))
+# print("B-A:{}".format(B - A))
+# print("A&B:{}".format(A & B))
+# print("A | B:{}".format(A | B))
+# print("A ^ B", format(A ^ B))
+
+
+# A={"p","y",123}
+# for item in A:
+#     print(item,end="")
+
+
+# A = {"p", "y", 123}
+# try:
+#     while True:
+#         print(A.pop(), end="")
+# except:
+#     pass
+
+# A = {"p", "y", 123}
+# print("p" in A)
+
+# 列表
+ls = ["p", "p", "y", "y", 123]
+s = set(ls)
+print(s)
+ls=list(s) #将集合转为列表
+print(ls)
